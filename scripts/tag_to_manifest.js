2f432cda3e95e90993a69ccf18ec0b04f1f9df3d
sonu sharma <echosonusharma@gmail.com>
2025-05-24 09:32:04 +0000

    fix: update manifest version + script

--- scripts/tag_to_manifest.js
+++ scripts/tag_to_manifest.js
@@ -1,5 +1,5 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
 
 const PATH_TO_MANIFEST = './app/manifest.json';
 
@@ -8,10 +8,10 @@ const PATH_TO_MANIFEST = './app/manifest.json';
     const lastTag = execSync('git describe --tags --abbrev=0').toString().trim();
     const version = lastTag.slice(1);
 
-    let manifest = JSON.parse(fs.readFileSync(PATH_TO_MANIFEST, { encoding: 'utf-8' }));
+    let manifest = JSON.parse(readFileSync(PATH_TO_MANIFEST, { encoding: 'utf-8' }));
     manifest.version = version;
     manifest = JSON.stringify(manifest, null, 2)
-    fs.writeFileSync(PATH_TO_MANIFEST, manifest, { encoding: 'utf-8' })
+    writeFileSync(PATH_TO_MANIFEST, manifest, { encoding: 'utf-8' })
   } catch (err) {
     console.error('Error getting last tag:', err.message);
   }
